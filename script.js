ac82830b7bab8a720bca7d823a752371d9896e80
Sharvari More <83008601+SharvariMore@users.noreply.github.com>
2022-02-06 22:48:26 +0530

    Added action for every tile in board

--- script.js
+++ script.js
@@ -13,5 +13,38 @@ window.addEventListener('DOMContentLoaded', () => {
     const PLAYERO_WON = 'PLAYERO_WON';
     const TIE = 'TIE';
 
+    /*
+    Index in board
+    [0][1][2]
+    [3][4][5]
+    [6][7][8]
+    */
+
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    //check what happens when user clicks on tile
+    const userAction = (tile, index) => {
+        if(isValidAction(tile) && isGameActive) {
+            tile.innerText = currentPlayer;
+            tile.classlist.add(`player${currentPlayer}`);
+            updateBoard(index);
+            handleResultValidation();
+            changePlayer();
+        }
+    }
+
+    tiles.forEach((tile,index) => {
+        tile.addEventListener('click', () => userAction(tile, index)); //when clicked on each tile it'll modify ui for tile & index for updating memory in array
+    });
+
     resetButton.addEventListener('click', resetBoard)
 });
