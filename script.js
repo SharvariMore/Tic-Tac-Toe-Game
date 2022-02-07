22ba59005810c76bd486eb699fcecf3ee5b24917
Sharvari More <83008601+SharvariMore@users.noreply.github.com>
2022-02-08 00:42:50 +0530

    Reset the board after declaring winner

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 window.addEventListener('DOMContentLoaded', () => {
-
     const tiles = Array.from(document.querySelectorAll('.tile'));
     const playerDisplay = document.querySelector('.display-player');
     const resetButton = document.querySelector('#reset');
     const announcer = document.querySelector('.announcer');
 
-    let board = ['','','','','','','','',''];
+    let board = ['', '', '', '', '', '', '', '', ''];
     let currentPlayer = 'X';
-    let isGameActive = true;      //show game result
+    let isGameActive = true;        //show game result
 
     const PLAYERX_WON = 'PLAYERX_WON';
     const PLAYERO_WON = 'PLAYERO_WON';
     const TIE = 'TIE';
 
+
     /*
-    Index in board
-    [0][1][2]
-    [3][4][5]
-    [6][7][8]
+        Index within the board
+        [0] [1] [2]
+        [3] [4] [5]
+        [6] [7] [8]
     */
 
     const winningConditions = [
@@ -36,42 +36,56 @@ window.addEventListener('DOMContentLoaded', () => {
         let roundWon = false;
         for (let i = 0; i <= 7; i++) {
             const winCondition = winningConditions[i];
-            const a = board[winCondition[0]];
+            const a = board[winCondition[0]];        //check if index is matching chars
             const b = board[winCondition[1]];
             const c = board[winCondition[2]];
-            if (a === '' || b === '' || c === '') {
+            if (a === '' || b === '' || c === '') {    //if empty board skip
                 continue;
             }
-            if (a === b && b === c) {
+            if (a === b && b === c) {          //if any one char matches with other char announce winner
                 roundWon = true;
                 break;
             }
         }
 
-        if (roundWon) {
-           announce(currentPlayer === 'X' ? PLAYERX_WON : PLAYERO_WON);
-           isGameActive = false;
-           return; 
+    if (roundWon) {
+            announce(currentPlayer === 'X' ? PLAYERX_WON : PLAYERO_WON);   //announce who won the game X or O
+            isGameActive = false;                  //clear the game
+            return;
         }
 
-        if (!board.includes('')) 
-            announce(TIE);
+    if (!board.includes(''))                      //if board is filled with no winner announce tie
+        announce(TIE);
     }
 
     //helper function to announce winner or end game state
     const announce = (type) => {
-        switch (type) {
+        switch(type){
             case PLAYERO_WON:
-                announcer.innerHTML = 'Player <span class="playerO"> O </span> Won';
+                announcer.innerHTML = 'Player <span class="playerO">O</span> Won';
                 break;
             case PLAYERX_WON:
-                announcer.innerHTML = 'Player <span class="playerX"> X </span> Won';
+                announcer.innerHTML = 'Player <span class="playerX">X</span> Won';
                 break;
             case TIE:
                 announcer.innerText = 'Tie';
         }
-        announcer.classList.remove('hide');       //display winner to user
+        announcer.classList.remove('hide');                   //display winner to user
     };
+
+    //makes sure that player enters value in empty tiles only
+    const isValidAction = (tile) => {
+        if (tile.innerText === 'X' || tile.innerText === 'O'){
+            return false;
+        }
+
+        return true;
+    };
+
+    //sets the value of element at given position in board array = value of current player variable
+    const updateBoard =  (index) => {
+        board[index] = currentPlayer;
+    }
 
     //change current player in board if X->O or O->X
     const changePlayer = () => {
@@ -85,16 +99,33 @@ window.addEventListener('DOMContentLoaded', () => {
     const userAction = (tile, index) => {
         if(isValidAction(tile) && isGameActive) {
             tile.innerText = currentPlayer;
-            tile.classlist.add(`player${currentPlayer}`);
+            tile.classList.add(`player${currentPlayer}`);
             updateBoard(index);
             handleResultValidation();
             changePlayer();
         }
     }
+    
+    //reset game state & board
+    const resetBoard = () => {
+        board = ['', '', '', '', '', '', '', '', ''];                      //create board with 9 empty strings
+        isGameActive = true;
+        announcer.classList.add('hide');
 
-    tiles.forEach((tile,index) => {
-        tile.addEventListener('click', () => userAction(tile, index)); //when clicked on each tile it'll modify ui for tile & index for updating memory in array
+        if (currentPlayer === 'O') {                                  //change player at each next click
+            changePlayer();
+        }
+
+        tiles.forEach(tile => {
+            tile.innerText = '';
+            tile.classList.remove('playerX');                       //remove player related classes
+            tile.classList.remove('playerO');
+        });
+    }
+
+    tiles.forEach( (tile, index) => {
+        tile.addEventListener('click', () => userAction(tile, index));
     });
 
-    resetButton.addEventListener('click', resetBoard)
+    resetButton.addEventListener('click', resetBoard);
 });
